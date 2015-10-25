8c7ea1a2042cf38f4f384726254fd9b6c5c0f7fa
Carlosbg <carlosbenitezgiuggia@gmail.com>
2015-10-25 14:49:38 -0300

    Primer commit

---
+++ Neftlix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Neftlix.Startup))]
+namespace Neftlix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
